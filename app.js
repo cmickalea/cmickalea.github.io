da865e8d7d72e7e30dbd9165d6e1104c3f18561c
mickalea <53543408+cmickalea@users.noreply.github.com>
2023-02-01 21:45:40 -0500

    Add Poetry app

---
+++ app.js
@@ -0,0 +1,12 @@
+const search = "https://poetrydb.org/title/"
+const searchButton = $('.search-by-book-title__submit')
+
+$(searchButton).on('click', function(event){
+    event.preventDefault()
+    let userSearchRequest = $('.search-by-book-title__input').val()
+    searchForPoem = userSearchRequest
+    fetch(`search${searchForPoem}`)
+    .then(res => res.json)
+    .then(data => console.log(data))
+})
+
